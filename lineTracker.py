9e30da4a419909c837ee0744349426ac077234af
Littlesamakfox <32133264+LittleSamakFox@users.noreply.github.com>
2020-12-09 12:15:19 +0900

    주석 설명

--- lineTracker.py
+++ lineTracker.py
@@ -6,16 +6,19 @@ ser = serial.Serial('/dev/serial/by-id/usb-Arduino_Srl_Arduino_Uno_7543931373735
 if ser is None:
     print("disconnected with arduino")
 
+#camera setting
 camera_pixel_x = 160
 camera_pixel_y = 120
 video_capture = cv2.VideoCapture(-1)
 video_capture.set(3, camera_pixel_x)
 video_capture.set(4, camera_pixel_y)
 
+#line coordinate factor
 gap = 1/10
 right_g = 1/2 + gap
 left_g = 1/2 - gap
 
+#stop time factor
 stop_count = 0
 
 while(True):
@@ -25,6 +28,7 @@ while(True):
 	# Crop the image
 	crop_img = frame[int(camera_pixel_y/2):camera_pixel_y, 0:camera_pixel_x]
 	
+	# Image Transformation for Recognition of black line
 	# convert to grayscale, gaussian blur, and threshold
 	gray = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
 	blur = cv2.GaussianBlur(gray,(5,5),0)
@@ -37,6 +41,7 @@ while(True):
 	#contours insert
 	contours,hierarchy = cv2.findContours(mask.copy(), 1, cv2.CHAIN_APPROX_NONE)
 	
+	#line detection
 	if len(contours) > 0:
 		c = max(contours, key=cv2.contourArea)
 		M = cv2.moments(c)
@@ -45,11 +50,9 @@ while(True):
 		
 		cv2.line(crop_img,(cx,0),(cx,720),(255,0,0),1)
 		cv2.line(crop_img,(0,cy),(1280,cy),(255,0,0),1)
-			
-		
 		cv2.drawContours(crop_img, contours, -1, (0,255,0), 1)
 		print(cx, cy)
-		
+		#Line recognition coordinates identify left, straight and right turns
 		if cx >= camera_pixel_x*(right_g):
 			print("R")
 			cmd = ("R").encode('ascii')
